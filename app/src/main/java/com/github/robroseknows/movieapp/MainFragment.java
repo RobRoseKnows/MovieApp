71d33704168cdb65edf47aafd13c2fa9d897f582
Robert Rose <bertra97@gmail.com>
2015-10-10 18:14:23 -0400

    more work

--- app/src/main/java/com/github/robroseknows/movieapp/MainFragment.java
+++ app/src/main/java/com/github/robroseknows/movieapp/MainFragment.java
@@ -5,15 +5,23 @@ import android.net.Uri;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
 
 
 public class MainFragment extends Fragment {
 
     private final String API_KEY = "hunter2";  // TODO: Remove before commit!!
-
+    private final String LOG_TAG = "MainFragment";
 
     public MainFragment() {
         // Required empty public constructor
@@ -30,9 +38,9 @@ public class MainFragment extends Fragment {
     private class MovieObject {
         private int movieId = 286217;
         private String movieTitle = "The Martian";
-        private double moviePopularity = 38.598673;
-        private double movieVoteAvg = 7.7;
+        private String movieDescription = "";
         private String moviePosterPath = "/AjbENYG3b8lhYSkdrWwlhVLRPKR.jpg";
+        private double movieVoteAvg = 7.7;
     }
 
     public class FetchMovieListTask extends AsyncTask<String, Void, String[]> {
@@ -44,6 +52,10 @@ public class MainFragment extends Fragment {
             }
 
             String[] out = new String[5];
+            String movieListRawJson = "";
+
+            HttpURLConnection urlConnection = null;
+            BufferedReader reader = null;
 
             try {
                 final String CALL_BASE_URL = "http://api.themoviedb.org/3/discover/movie";
@@ -52,13 +64,62 @@ public class MainFragment extends Fragment {
 
 
                 Uri builtUri = Uri.parse(CALL_BASE_URL).buildUpon()
-                        .appendQueryParameter()
-                )
-            }
+                        .appendQueryParameter(SORTBY_PARAM, params[0])
+                        .appendQueryParameter(API_PARAM, API_KEY)
+                        .build();
+                URL url = new URL(builtUri.toString());
+                urlConnection = (HttpURLConnection) url.openConnection();
+                urlConnection.setRequestMethod("GET");
+                urlConnection.connect();
+
+                InputStream inputStream = urlConnection.getInputStream();
+                StringBuffer buffer = new StringBuffer();
+                if(inputStream == null) {
+                    return null;
+                }
+
+                reader = new BufferedReader(new InputStreamReader(inputStream));
+
+                String line;
+                while((line = reader.readLine()) != null) {
+                    buffer.append(line + "\n");
+                }
+
+                if(buffer.length() == 0) {
+                    return null;
+                }
+
+                movieListRawJson = buffer.toString();
+
+            } catch(IOException e) {
+                Log.e(LOG_TAG, "Error: ", e);
+                return null;
+            } finally {
+                if(urlConnection != null) {
+                    urlConnection.disconnect();
+                }
+
+                if(reader != null) {
+                    try {
+                        reader.close();
+                    } catch(final IOException e) {
+                        Log.e(LOG_TAG, "Error closing stream", e);
+                    }
+                }
+        }
 
             ///?sort_by=popularity.desc&api_key=
 
             return out;
+        }
+
+        private String[] getListDataFromJson(String movieListJsonStr) {
+            final String OWM_ID = "id";
+            final String OWM_TITLE = "original_title";
+            final String OWM_DESC = "overview";
+            final String OWM_POSTER = "poster_path";
+            final String OWM_VOTE = "vote_average";
+
         }
     }
 
